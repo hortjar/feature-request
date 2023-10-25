3da5a831d2c11af82cc1b1166eb81105dbf7f6dd
Jaroslav Hort <hortjaroslav0@gmail.com>
2023-10-25 16:39:37 +0200

    feat: add radix ui themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import "~/styles/globals.css";
+import "@radix-ui/themes/styles.css";
 
 import { Inter } from "next/font/google";
 import { headers } from "next/headers";
 
 import { TRPCReactProvider } from "~/trpc/react";
+import { Theme } from "@radix-ui/themes";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -22,9 +24,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`font-sans ${inter.variable}`}>
-        <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
+        <Theme appearance="dark">
+          <TRPCReactProvider headers={headers()}>{children}</TRPCReactProvider>
+        </Theme>
       </body>
     </html>
   );
