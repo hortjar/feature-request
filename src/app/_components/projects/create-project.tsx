8f435a816a489b9b56b410ac990262870b267973
Jaroslav Hort <hortjaroslav0@gmail.com>
2023-11-01 14:42:03 +0100

    feat: create project as react hook form

--- src/app/_components/projects/create-project.tsx
+++ src/app/_components/projects/create-project.tsx
@@ -1,32 +1,43 @@
 "use client";
 
+import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@radix-ui/themes";
 import { useRouter } from "next/navigation";
-import { useRef } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import { api } from "~/trpc/react";
 
+const schema = z.object({
+  name: z.string().min(1).max(255),
+});
+
+type FormData = z.infer<typeof schema>;
+
 export default function CreateProject() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting, isDirty, isValid },
+  } = useForm<FormData>({ resolver: zodResolver(schema) });
   const router = useRouter();
 
-  const nameRef = useRef<HTMLInputElement>(null);
-
   const createProject = api.project.create.useMutation({
-    onSuccess: (data) => {
-      router.push("/project/" + data);
+    onSuccess: (id) => {
+      router.push("/project/" + id);
     },
   });
 
+  async function onSubmit(data: FormData) {
+    console.log(data);
+    await createProject.mutateAsync({
+      name: data.name,
+    });
+  }
+
   return (
-    <form
-      className="flex flex-col gap-3"
-      onSubmit={(e) => {
-        e.preventDefault();
-        createProject.mutate({
-          name: nameRef.current!.value,
-        });
-      }}
-    >
-      <input type="text" ref={nameRef} />
+    <form className="flex flex-col gap-3" onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" {...register("name", { required: true })} />
+      {errors?.name && <p>Name cannot be null</p>}
       <Button type="submit">Create</Button>
     </form>
   );
