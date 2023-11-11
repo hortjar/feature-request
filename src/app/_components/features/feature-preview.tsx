d93f401052f6a961c12ea93855ee0de78c171401
Jaroslav Hort <hortjaroslav0@gmail.com>
2023-11-11 09:07:18 +0100

    feat: feature preview button style

--- src/app/_components/features/feature-preview.tsx
+++ src/app/_components/features/feature-preview.tsx
@@ -3,6 +3,8 @@ import { getServerAuthSession } from "~/server/auth";
 import { FeatureRating } from "./feature-rating";
 import { type FC } from "react";
 import { type Feature } from "~/server/db/types";
+import { ChevronRightIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
 
 interface FeatureProps {
   feature: Feature;
@@ -10,23 +12,34 @@ interface FeatureProps {
 
 export const FeaturePreview: FC<FeatureProps> = async (props) => {
   const session = await getServerAuthSession();
+  const iconSize = 25;
 
   return (
-    <div
-      key={props.feature.id}
-      className="grid grid-cols-[auto_1fr] grid-rows-[2rem_1fr] gap-1"
+    <Link
+      href={"feature/" + props.feature.id}
+      className="rounded-lg py-3 pr-3 ring-slate-700 transition duration-100 hover:ring"
     >
-      <div className="col-start-1 row-span-2 row-start-1 items-center justify-center">
-        <FeatureRating
-          featureId={props.feature.id}
-          ratings={props.feature.ratings}
-          userId={session?.user.id}
-        />
+      <div
+        key={props.feature.id}
+        className="grid grid-cols-[auto_1fr_auto] grid-rows-[2rem_1fr] gap-1"
+      >
+        <div className="col-start-1 row-span-2 row-start-1 items-center justify-center">
+          <FeatureRating
+            featureId={props.feature.id}
+            ratings={props.feature.ratings}
+            userId={session?.user.id}
+          />
+        </div>
+        <Heading className="col-start-2 row-start-1">
+          {props.feature.name}
+        </Heading>
+        <div className="col-start-2 row-start-2">{props.feature.content}</div>
+        <div className="col-start-3 row-span-2 row-start-1 items-center justify-center">
+          <div className="flex h-full items-center justify-center">
+            <ChevronRightIcon width={iconSize} height={iconSize} />
+          </div>
+        </div>
       </div>
-      <Heading className="col-start-2 row-start-1">
-        {props.feature.name}
-      </Heading>
-      <div className="col-start-2 row-start-2">{props.feature.content}</div>
-    </div>
+    </Link>
   );
 };
